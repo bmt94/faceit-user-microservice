44da8394946ee9ccabfabf0f4cf1bdd64859a0a7
bmt94 <daleel94@live.co.uk>
2019-12-11 01:03:32 +0000

    end points for modify. add, delete. seperated controllers

--- user-microservice/src/main/java/com/faceit/usermicroservice/controllers/UserCRUDController.java
+++ user-microservice/src/main/java/com/faceit/usermicroservice/controllers/UserCRUDController.java
@@ -7,12 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faceit.usermicroservice.entities.User;
@@ -20,19 +21,37 @@ import com.faceit.usermicroservice.repositories.user_repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
-public class ModifyUserController {
+public class UserCRUDController {
 	
 	@Autowired
 	UserRepository userRepo;
 	
-	@PutMapping(value = "/modify")
-	public ResponseEntity<User> modifyUser(@RequestBody User user, HttpServletRequest request){	
-//		Optional<User> user = userRepo.findById(userID); 
-//		if(user.isPresent()) return ResponseEntity.ok(user.get());
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		return null;
-	}
 
+	
+	@GetMapping(value = "/view/{userID}")
+	public ResponseEntity<User> getUserByID(@PathVariable(value = "userID") int userID, HttpServletRequest request){	
+		Optional<User> user = userRepo.findById(userID); 
+		if(user.isPresent()) return ResponseEntity.ok(user.get());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+	
+	@PostMapping(value = "/add")
+	public void addUser(@RequestBody User user, HttpServletRequest request){	
+		userRepo.save(user);
+	}
+	
+	@PutMapping(value = "/modify")
+	public void modifyUser(@RequestBody User user, HttpServletRequest request){	
+		userRepo.updateUser(user);
+	}
+	
+	@DeleteMapping(value = "/remove")
+	public void deleteUser(@RequestBody User user, HttpServletRequest request){	
+		userRepo.delete(user);
+	}	
+	
+	
+	
 	
 }
 
--- user-microservice/src/main/java/com/faceit/usermicroservice/controllers/UserFilteredController.java
+++ user-microservice/src/main/java/com/faceit/usermicroservice/controllers/UserFilteredController.java
@@ -1,14 +1,10 @@
 package com.faceit.usermicroservice.controllers;
 
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +14,11 @@ import com.faceit.usermicroservice.repositories.user_repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
-public class GetUserController {
+public class UserFilteredController {
 	
 	@Autowired
 	UserRepository userRepo;
-	
-	@GetMapping(value = "/view/{userID}")
-	public ResponseEntity<User> getUserByID(@PathVariable(value = "userID") int userID, HttpServletRequest request){	
-		Optional<User> user = userRepo.findById(userID); 
-		if(user.isPresent()) return ResponseEntity.ok(user.get());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-	}
+
 	
 	@GetMapping(value = "/list")
 	public ResponseEntity<Iterable<User>> getAllUsers(HttpServletRequest request){
@@ -71,6 +61,7 @@ public class GetUserController {
 		Iterable<User> users = userRepo.findByEmail(email);
 		return ResponseEntity.ok(users);
 	}
+	
 	
 	
 }
